14b701f3ac58311ac6014dcbaa6058acbf0a33cc
frajavimopu <frajavimopu@gmail.com>
2026-02-06 11:04:41 -0600

    Inicio de proyecto ITESS-TICS POO

---
+++ src/tema1ide/HelloWorld.java
@@ -0,0 +1,18 @@
+/*
+*  2026 ITESS-TICS 
+*  Periodo: Enero-Junio
+*  Asignatura: Programación Orientada a Objetos
+*  Docente: Francisco Javier Montecillo Puente
+*  Descripción: Tema 1. Entorno de desarollo
+*               Prueba del entorno de desarrollo y  github
+*  Programador: FJMP
+*  Fecha: 06 febrero 2026
+*/
+
+package tema1ide;
+
+public class HelloWorld {
+    public void sayHello(String name) {
+        System.out.println("Hello, " + name);
+    }
+}
---
+++ src/tema1ide/HelloWorldApp.java
@@ -0,0 +1,22 @@
+/*
+*  2026 ITESS-TICS 
+*  Periodo: Enero-Junio
+*  Asignatura: Programación Orientada a Objetos
+*  Docente: Francisco Javier Montecillo Puente
+*  Descripción: Tema 1. Entorno de desarollo
+*               Prueba del entorno de desarrollo y  github
+*  Programador: FJMP
+*  Fecha: 06 febrero 2026
+*/
+
+package tema1ide;
+
+
+public class HelloWorldApp {
+    public static void main(String [] args) {
+            HelloWorld  hello = new HelloWorld();
+
+            hello.sayHello("FJMP");
+            hello.sayHello("ITESS");
+    }
+}
